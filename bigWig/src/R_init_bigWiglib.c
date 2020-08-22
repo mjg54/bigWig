f817bd2506cb0861be60782c622a0e72b7848a33
mjg54 <guertinmj@gmail.com>
2020-08-21 22:44:51 -0400

    Update R_init_bigWiglib.c

--- bigWig/src/R_init_bigWiglib.c
+++ bigWig/src/R_init_bigWiglib.c
@@ -15,7 +15,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void R_init_bigWig(DllInfo *dll)
+void R_init_bigWiglib(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
